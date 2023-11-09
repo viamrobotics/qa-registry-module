c333c68fe73508bc6d2b5b6d516efbf8b5756fcb
Michael Lee <michaellee1019@gmail.com>
2023-11-09 14:10:49 -0500

    initial commit

---
+++ hi_model.py
@@ -0,0 +1,19 @@
+import asyncio
+from typing import Any, ClassVar, Dict, Mapping, Optional
+from viam.components.sensor import Sensor
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import ResourceName
+from viam.resource.base import ResourceBase
+from viam.resource.types import Model, ModelFamily
+
+class MySensor(Sensor):
+    # Subclass the Viam Arm component and implement the required functions
+    MODEL: ClassVar[Model] = Model(ModelFamily("viam", "qa-registry-module"), "hi")
+
+    @classmethod
+    def new(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
+        sensor = cls(config.name)
+        return sensor
+
+    async def get_readings(self, extra: Optional[Dict[str, Any]] = None, **kwargs) -> Mapping[str, Any]:
+            return {"word": "hi!"}
